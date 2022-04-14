9266aa93e6d49bdb70c0cf2d16afe5d845820d5a
Kasim Ridwan <kasimsheyi20@gmail.com>
2022-04-14 10:39:33 +0100

    structuring of the layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,18 @@
 </head>
 
 <body>
-    @yield('content')
+    <div class="container-fluid">
+        <div class="row">
+            @include('partials.sidebar')
+            <div class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
+                @include('partials.navbar')
+                <div class="container">
+                    @yield('content')
+                </div>
+                @include('partials.footer')
+            </div>
+        </div>
+    </div>
     <!-- JavaScript Bundle with Popper -->
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-light bg-light p-3">
+    <div class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
+        <a class="navbar-brand" href="#">
+            Users
+        </a>
+        <button class="navbar-toggler d-md-none collapsed mb-3" type="button" data-toggle="collapse"
+            data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="col-12 col-md-4 col-lg-2">
+        <input class="form-control form-control-dark" type="text" placeholder="Search" aria-label="Search">
+    </div>
+</nav>
